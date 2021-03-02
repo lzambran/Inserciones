3e83c1988b5fc165c19a098a1f50bba517e2b3b5
lzambran <76109631+lzambran@users.noreply.github.com>
2021-03-02 08:12:18 -0500

    Insercion de datos para el trigger

---
+++ Inserciones.sql
@@ -0,0 +1,13 @@
+
+--Insercion de datos para el trigger
+
+--Tabla Empleado
+insert into Empleado values (8,1,1,'Luis','Zambrano','Las Gilces','0978716971','chofer','1314487222');
+insert into Empleado values (2,1,2,'Juan','Alcivar','Crucita','0998751423','Maquinista','1309874532');
+insert into Empleado values (1,1,3,'Pedro','Demera','Portoviejo','0987563214','Embalador','1308964781');
+
+--Tabla Pago
+
+insert into Pago values (1,300,'17/2/2021');
+insert into Pago values (2,400,'17/2/2021');
+insert into Pago values (3,300,'17/2/2021');
